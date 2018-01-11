1aa5c023e36b23cb337f5d8c5d75a3a3a4ea8066
thomas madlener <thomas.madlener@cern.ch>
2018-01-11 13:29:10 +0100

    Add generic makefile and add necessary setup to setup script

--- setup.sh
+++ setup.sh
@@ -9,3 +9,8 @@ export CHIB_CHIC_POLFW_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 ## add the python directory to the python search path
 ## NOTE: 'cmsenv'-ing a CMSSW release completely resets the PYTHON_PATH, so that it might be necessary to call this script again after setting it up
 PYTHONPATH=$PYTHONPATH:${CHIB_CHIC_POLFW_DIR}/python
+
+## setup up a common root-version and a common gcc compiler so that the variables can then be used in makefiles, etc.
+## NOTE: currently uses the default setup of the environment
+export ROOT_CONFIG_BIN=$(which root-config)
+export GCC_BASE_DIR=""
