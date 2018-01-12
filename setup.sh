5e2a1dcc380007bb4a6e52b913d7fe3c4e9b8ceb
thomas madlener <thomas.madlener@cern.ch>
2018-01-12 15:39:50 +0100

    Make setup check first if setup already happened

--- setup.sh
+++ setup.sh
@@ -3,14 +3,20 @@
 ## setup script for setting up some environment and path variables.
 ## TODO: Find common setup, that works with all the python packages and ROOT etc. and put the initialization here.
 
-## get the absolute directory of this script, regardless of where it's called from
-export CHIB_CHIC_POLFW_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+# first check if setup already happened, and only do it if not.
+# In this way, this script can be sourced multiple times or from within bash scripts
+if [[ -z "${CHIB_CHIC_POLFW_DIR+x}" ]]; then
 
-## add the python directory to the python search path
-## NOTE: 'cmsenv'-ing a CMSSW release completely resets the PYTHON_PATH, so that it might be necessary to call this script again after setting it up
-PYTHONPATH=$PYTHONPATH:${CHIB_CHIC_POLFW_DIR}/python
+    ## get the absolute directory of this script, regardless of where it's called from
+    export CHIB_CHIC_POLFW_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-## setup up a common root-version and a common gcc compiler so that the variables can then be used in makefiles, etc.
-## NOTE: currently uses the default setup of the environment
-export ROOT_CONFIG_BIN=$(which root-config)
-export GCC_BASE_DIR=""
+    ## add the python directory to the python search path
+    ## NOTE: 'cmsenv'-ing a CMSSW release completely resets the PYTHON_PATH, so that it might be necessary to call this script again after setting it up
+    PYTHONPATH=$PYTHONPATH:${CHIB_CHIC_POLFW_DIR}/python
+
+    ## setup up a common root-version and a common gcc compiler so that the variables can then be used in makefiles, etc.
+    ## NOTE: currently uses the default setup of the environment
+    export ROOT_CONFIG_BIN=$(which root-config)
+    export GCC_BASE_DIR=""
+
+fi
