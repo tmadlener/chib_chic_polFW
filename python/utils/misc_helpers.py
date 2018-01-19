613f429a4249c2457d4f98d1fd11588cb94c29fa
thomas madlener <thomas.madlener@cern.ch>
2018-01-19 11:53:37 +0100

    Add stringify function and random string generation function.
    - Stringify function can be used to turn a selection string into a
      viable histogram name (such that it can still be used in TTree::Draw()
      and it is also possible to reverse it and get back the original
      string
    - random string generation for creating ROOT objects that are
      identified by their name by ROOT

--- python/utils/misc_helpers.py
+++ python/utils/misc_helpers.py
@@ -3,8 +3,9 @@ Module for small miscellaneous helper functions that are intended to handle
 things concerning os or python built-ins.
 """
 
-
 import os
+from random import choice
+from string import ascii_letters, digits
 
 def cond_mkdir(path):
     """
@@ -33,3 +34,58 @@ def cond_mkdir_file(filename):
     """
     path, _ = os.path.split(filename)
     cond_mkdir(path)
+
+
+def stringify(selection, reverse=False):
+    """Get a string representation of a selection string
+
+    Get a string representation without any special characters from a selection
+    as it is used e.g. in TTree::Draw or in RooDataSet::reduce.
+
+    Args:
+        selection (str): Selection string
+        reverse (bool, optional): If true, the original representation can be
+            obtained (with stripped whitespace)
+
+    Returns:
+        str
+    """
+    repl_pairs = (
+        ('>=', '_ge_'),
+        ('<=', '_le_'),
+        ('<', '_lt_'),
+        ('>', '_gt_'),
+        ('==', '_eq_'),
+        ('!=', '_ne_'),
+        ('&&', '_AND_'),
+        ('||', '_OR_'),
+        ('.', 'p'),
+        ('&', '_BITAND_'),
+        ('(', '_PAROP_'), # Rather ugly and hacky solution to handle parenthesis
+        (')', '_PARCL_')  # without usage of regular expressions
+    )
+
+    ret_str = selection.replace(' ', '')
+    if reverse:
+        for rep, sym in repl_pairs:
+            ret_str = ret_str.replace(sym, rep)
+    else:
+        for sym, rep in repl_pairs:
+            ret_str = ret_str.replace(sym, rep)
+
+    return ret_str
+
+
+def create_random_str(length=32):
+    """
+    Create a random string containing upper and lower case ASCII characters and
+    numbers
+
+    Implementation following: https://stackoverflow.com/questions/2257441/
+    Args:
+        length (int, optional): length of the output string
+
+    Returns:
+        str: Non-cryptographically safe random string
+    """
+    return '.'.join(choice(ascii_letters + digits) for _ in xrange(length))
