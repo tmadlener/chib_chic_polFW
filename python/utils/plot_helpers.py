tmadlener/chib_chic_polFW

commit b5eded1ff1c3a401fa7551efd1457a880c41f21f

Add histogram plotting script (first version)
Add a python script that does the second step of the plotting (i.e. the
actual plotting), after another has done the collecting of the histograms.
Script generates one plot per run, where the contents of the plots can be set
via cl args. To facilitate creating all plots an additional bash script has
been addeded as well.

Histograms are retreived from the root file created by the collect_histograms.py
script via the PlotServer class, that is basically nothing else than a fancy
wrapper that keeps the root file alive for the whole process and provides some
'simple' access to the histograms stored in the file (assuming they have been
stored via collect_histograms.py.

- mkplot has been adapted to handle empty lists and also is able to set axis
labels now.
- plot_on_canvas can now handle different options for drawing and putting
objects to the legend for more fine grained control (default behavior stays
the same)
- tdr_style with larger hatches spacing and thiner hatches lines to be able to
plot fill styles using hatches (old settings produced lines that were too thick
to be seen as single lines)

python/utils/plot_helpers.py
@@ -14,7 +14,7 @@ import ROOT as r
 
 from collections import Iterable
 from utils.misc_helpers import create_random_str
-from utils.hist_utils import set_common_range
+from utils.hist_utils import set_common_range, set_labels
 
 _colors = []
 _color_indices = []
@@ -154,9 +154,12 @@ def plot_on_canvas(can, plots, **kwargs):
 
     get_att = lambda i: attributes[ i % len(attributes) ]
 
+    leg_option = kwargs.pop('legOpt', '')
+    draw_option = kwargs.pop('drawOpt')
+    if not leg_option:
+        leg_option = draw_option
+    draw_option = ''.join(['same', draw_option])
 
-    leg_option = kwargs.pop('drawOpt', '')
-    draw_option = ''.join(['same', leg_option])
     legend = kwargs.pop('leg', None)
     leg_entries = kwargs.pop('legEntries', [h.GetName() for h in plots])
 
@@ -187,7 +190,9 @@ def mkplot(pltables, **kwargs):
     Plot all pltables onto a canvas and return the canvas
 
     Args:
-        pltables: single plotable ROOT object or list of plotable ROOT objects
+        pltables: single plotable ROOT object or list of plotable ROOT objects.
+            If this is an empty list an empty canvas (or the passed in canvas)
+            will be returned
 
     Keyword Args:
         colors (list. optional): list of colors to be used for plotting
@@ -201,16 +206,26 @@ def mkplot(pltables, **kwargs):
             a None value or even two None values. For any passed value that
             value will be used as axis range, for any None value an appropriate
             value will be determined from the passed pltables
+        can (ROOT.TCanvas): Do not create new canvas but use passed canvas to
+            plot on
 
     Returns:
-        ROOT.TCanvas: Canvas with all the plotables drawn onto it
+        ROOT.TCanvas: Canvas with all the plotables drawn onto it.
     """
     # allow single plots to be handled the same as a list of plots
     if not isinstance(pltables, Iterable):
         pltables = [pltables]
 
-    can_name = create_random_str()
-    can = r.TCanvas(can_name, '', 50, 50, 600, 600)
+    can = kwargs.pop('can', None)
+    if can is None:
+        can_name = create_random_str()
+        can = r.TCanvas(can_name, '', 50, 50, 600, 600)
+
+    # Check if at least one pltable has been passed and return the canvas if not
+    # NOTE: Can't return earlier with None, since a canvas is expected to be
+    # returned, and only here is it certain, that we return the right canvas
+    if len(pltables) < 1:
+        return can
 
     only_hists = all(p.InheritsFrom('TH1') for p in pltables)
 
@@ -220,6 +235,12 @@ def mkplot(pltables, **kwargs):
     y_range = kwargs.pop('yRange', None)
     if y_range is not None and only_hists:
         set_common_range(pltables, axis='y', dscale=0.05, drange=y_range)
+
+    y_label = kwargs.pop('yLabel', '')
+    x_label = kwargs.pop('xLabel', '')
+    if (y_label or x_label) and only_hists:
+        for h in pltables:
+            set_labels(h, x_label, y_label)
 
     plot_on_canvas(can, pltables, **kwargs)
     return can

polarization/plotting/make_all_plots
@@ -1,0 +1,57 @@
+#!/bin/bash
+set -e
+
+INPUTFILE=./histograms.root
+OUTPUTBASE=./plots_test/
+FRAMES=(HX CS)
+NORM_AT_0="-nz" # set to "-nz" or "--normalize-at-zero" to normalize hists at 0
+
+mkdir -p ${OUTPUTBASE}
+
+
+## create associative array for 2016 triggers (pt bin to trigger)
+declare -A TRIG_2016_PT=( [1]=Dimuon10_Jpsi_Barrel \
+                              [2]=Dimuon16_Jpsi \
+                              [3]=Dimuon20_Jpsi \
+                              [4]=Dimuon20_Jpsi \
+                              [5]=Dimuon16_Jpsi ) # TODO: Use Dimuon20 when data available
+
+TRIG_2012=Dimuon8_Jpsi
+INPUT_2012_DATA="2012:${TRIG_2012}:data:8 TeV"
+INPUT_2012_MC="2012:${TRIG_2012}:mc:8 TeV (MC)"
+
+VARIABLES=( costh cosalpha phi )
+PT_BINS=$(echo {1..5})
+
+## make the ratio and dist plots for a given input
+function ratio_dist_plots() {
+    var=${1}
+    frame=${2}
+    ptbin=${3}
+    output_base=${4}
+
+    INPUT_2016_DATA="2016:${TRIG_2016_PT[${ptbin}]}:data:13 TeV"
+
+    input="--input=""\"${INPUT_2012_DATA}"\"" --input=""\"${INPUT_2012_MC}"\"" --input=""\"${INPUT_2016_DATA}"\"""
+
+    plotter=${CHIB_CHIC_POLFW_DIR}/polarization/plotting/make_plot.py
+
+    ${plotter} --input="${INPUT_2012_DATA}" --input="${INPUT_2012_MC}" --input="${INPUT_2016_DATA}" \
+               --variable=${var} --frame=${frame} --ptbin=${ptbin} --output=${output_base}_ratio.pdf \
+               ${NORM_AT_0} --ratio ${INPUTFILE}
+
+    ${plotter} --input="${INPUT_2012_DATA}" --input="${INPUT_2012_MC}" --input="${INPUT_2016_DATA}" \
+               --variable=${var} --frame=${frame} --ptbin=${ptbin} --output=${output_base}_dists.pdf \
+               ${NORM_AT_0} --dist ${INPUTFILE}
+
+}
+
+for var in ${VARIABLES[@]}; do
+    for frame in ${FRAMES[@]}; do
+        for pt in ${PT_BINS[@]}; do
+            outbase=${OUTPUTBASE}/${var}_${frame}_pt${pt}
+
+            ratio_dist_plots ${var} ${frame} ${pt} ${outbase}
+        done
+    done
+done

polarization/plotting/make_plot.py
@@ -1,0 +1,288 @@
+#!/usr/bin/env python
+"""
+Script for creating a plot with multiple inputs
+"""
+
+import re
+
+import ROOT as r
+r.PyConfig.IgnoreCommandLineOptions = True
+r.gROOT.SetBatch()
+
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s - %(funcName)s: %(message)s')
+
+from utils.plot_helpers import mkplot, default_colors
+from utils.setup_plot_style import set_TDR_style
+from utils.PlotServer import PlotServer
+from utils.hist_utils import get_y_max
+
+# values at which chic1 and chic2 should be normalized to at 0 if that
+# normalization option is chosen
+chic1_norm_0 = 1.0
+chic2_norm_0 = 0.5
+
+
+def get_plot_attributes(year, dmc, plot):
+    """
+    Get the plot attributes for a given plot
+    """
+    year_colors = {
+        '2012': 1,
+        '2016': default_colors()[0],
+        '2017': default_colors()[1]
+    }
+    marker_style = {'2012': 20, '2016': 22, '2017': 23}
+    marker_style_open = {'2012': 24, '2016': 26, '2017': 32}
+    fill_style = {'chic1': 3354, 'chic2': 3345 }
+    size = 2
+    linewidth = 2
+
+    color = year_colors[year]
+    if plot == 'ratio' or plot == 'chic2':
+        marker = marker_style[year]
+    else:
+        marker = marker_style_open[year]
+
+    attr = {'color': color, 'marker': marker, 'size': size, 'width': linewidth}
+    if dmc == 'mc':
+        attr['size'] = 0 # do not plot markers for mc
+        attr['width'] = 0
+        if plot == 'ratio':
+            attr['fillalpha'] = (color, 0.5)
+        else:
+            attr['fillstyle'] = fill_style[plot]
+
+    return attr
+
+
+def setup_legend(frame, ratio):
+    """
+    Setup the legend
+    """
+    position = {
+        'CS': [0.2, 0.6, 0.65, 0.75],
+        'HX': [0.5, 0.65, 0.95, 0.8],
+        'PX': [0.6, 0.5, 0.9, 0.65] # TODO: only CS and HX "optimized" for now
+    }
+    # TODO: dynamic placement
+    leg = r.TLegend(*position[frame])
+    leg.SetFillColor(r.kWhite)
+    leg.SetTextFont(42)
+    leg.SetTextSize(0.04)
+    leg.SetEntrySeparation(0.01)
+    leg.SetBorderSize(0)
+    return leg
+
+
+def normalize_histos(hchic1, hchic2, norm_mode):
+    """
+    Normalize the chic1 and chic2 histograms according to a certain
+    normalization mode.
+
+    Args:
+        hchic1, hchic2 (ROOT.TH1): Histogram that are normalized assuming they
+            belong together (i.e. are from the same year)
+        norm_mode (str): Has to be one of the following:
+            - 'nsignal': both histograms are normalized by the combined sum of
+              their integrals
+            - 'at0': both histograms are normalized according to their value at
+              0. The chic1 histograms will be normalized to 1, the chic2
+              histograms will be normalized to 0.5
+    """
+    logging.debug('Normalizing histograms {} and {} with mode {}'
+                      .format(hchic1.GetName(), hchic2.GetName(), norm_mode))
+    if norm_mode == 'nsignal':
+        n_signal = hchic1.Integral() + hchic2.Integral()
+        if n_signal <= 0:
+            logging.warning('sum of integrals is 0 in normalizing histograms: '
+                            '{} and {}'
+                            .format(hchic1.GetName(), hchic2.GetName()))
+        else:
+            hchic1.Scale(1.0 / n_signal)
+            hchic2.Scale(1.0 / n_signal)
+    elif norm_mode == 'at0':
+        get_bin_0 = lambda h: h.GetBinContent(h.FindBin(0))
+        n_chic1 = get_bin_0(hchic1)
+        if n_chic1 > 0:
+            hchic1.Scale(chic1_norm_0 / n_chic1)
+        n_chic2 = get_bin_0(hchic2)
+        if n_chic2 > 0:
+            hchic2.Scale(chic2_norm_0 / n_chic2)
+
+        if n_chic1 <= 0 or n_chic2 <= 0:
+            logging.warning('bin contents at 0 were {} and {} for histograms: '
+                            '{} and {}'
+                            .format(n_chic1, n_chic2,
+                                    hchic1.GetName(), hchic2.GetName()))
+    else:
+        logging.warning('norm_mode was not a valid option. Did not normalize '
+                         'histograms {} and {}'
+                         .format(hchic1.GetName(), hchic2.GetName()))
+
+
+def get_plot_hists(pserver, trg_years, ratio, var, frame, pt,
+                   norm_mode='nsignal'):
+    """
+    Get all plots that are necessary for the desired plot
+
+    Args:
+        trg_years (list of tuples): each year needs an accompanying trigger
+            and wether it is data or mc
+    """
+    hists = {}
+    for year, trg, dmc in trg_years: # discard leg entries for now
+        if ratio:
+            hist = pserver.get_hist(dmc, year, trg, pt, var, frame, 'ratio')
+            if hist is not None:
+                hists[(year, dmc, 'ratio')] = hist
+        else:
+            hist = pserver.get_hist(dmc, year, trg, pt, var, frame, 'chic1')
+            hists[(year, dmc, 'chic1')] = hist
+            hist = pserver.get_hist(dmc, year, trg, pt, var, frame, 'chic1')
+            hists[(year, dmc, 'chic2')] = hist
+
+            normalize_histos(hists[(year, dmc, 'chic1')],
+                             hists[(year, dmc, 'chic2')], norm_mode)
+
+    return hists
+
+
+def get_trigger_year_info(inputlist):
+    """
+    Parse the arguments from the --input list and return necessary info
+    """
+    logging.debug('Parsing input arguments: {}'.format(inputlist))
+    ids = []
+    leg_entries = {}
+
+    for inp in inputlist:
+        logging.debug('Parsing input: {}'.format(inp))
+        args = inp.split(':')
+        if len(args) == 3:
+            args.append('') # append empty leg entry
+        if len(args) >= 4:
+            year, trg, data = args[:3]
+            ids.append((year, trg, data),)
+            leg_id = (year, data),
+            leg_entries[leg_id] = args[3]
+        else:
+            logging.warning('Could not obtain all the necessary information '
+                            'from {}. Skipping this input'.format(inp))
+
+    return ids, leg_entries
+
+
+def nice_leg_entries(leg_entries):
+    """Replace dict strings with nicer plotting strings"""
+    for i, entry in enumerate(leg_entries):
+        leg_entries[i] = re.sub(r'chic(\d)', r'#chi_{c\1}', entry)
+
+    return leg_entries
+
+
+def get_xlabel(var, frame):
+    """Get nice x-labels"""
+    if var == 'phi':
+        return '#phi^{{{0}}}_{{folded}}'.format(frame)
+    x_label = var.replace('alpha', r'#alpha').replace('th', r'#theta')
+    return '|{0}^{{{1}}}|'.format(x_label, frame)
+
+
+def main(args):
+    """Main"""
+    # need PlotServer here, to keep the root file open
+    pserver = PlotServer(args.histfile)
+
+    norm_mode = 'at0' if args.normalize_at_zero else 'nsignal'
+
+    year_trg_ids, leg_entries = get_trigger_year_info(args.input)
+    hists = get_plot_hists(pserver, year_trg_ids, args.ratio, args.variable,
+                           args.frame, args.ptbin, norm_mode)
+
+    # split into data and mc hist due to different plotting styles for the two
+    data_hists = {k: hists[k] for k in hists if 'data' in k}
+    mc_hists = {k: hists[k] for k in hists if 'mc' in k}
+
+    leg = None
+    data_leg = []
+    mc_leg = []
+    if args.legend:
+        leg = setup_legend(args.frame, args.ratio )
+        if args.ratio:
+            data_leg = [leg_entries[(k[0], k[1]),] for k in data_hists]
+            mc_leg = [leg_entries[(k[0], k[1]),] for k in mc_hists]
+        else:
+            data_leg = [', '.join([leg_entries[(k[0], k[1]),], k[2]])
+                        for k in data_hists]
+            data_leg = nice_leg_entries(data_leg)
+            mc_leg = [', '.join([leg_entries[(k[0], k[1]),], k[2]])
+                        for k in mc_hists]
+            mc_leg = nice_leg_entries(mc_leg)
+
+
+    dpl_attr = [get_plot_attributes(*k) for k in data_hists]
+    mpl_attr = [get_plot_attributes(*k) for k in mc_hists]
+
+    y_label = '#chi_{c2} / #chi_{c1}'
+    if not args.ratio:
+        if not args.normalize_at_zero:
+            y_label = 'events / sum of signal #chi_{c1} and #chi_{c2} events'
+        else:
+            y_label = 'events (arb. units)'
+    x_label = get_xlabel(args.variable, args.frame)
+
+    # to have same y-range for everything, have to do it manually, as
+    # mkplot can't handle it in two different calls
+    y_max = get_y_max(data_hists.values() + mc_hists.values()) * 1.1
+
+    set_TDR_style()
+    can = mkplot(data_hists.values(), yRange=[0, y_max], drawOpt='E1',
+                 attributes=dpl_attr, leg=leg, legEntries=data_leg,
+                 legOpt='PLE', xLabel=x_label, yLabel=y_label)
+    can = mkplot(mc_hists.values(), yRange = [0, y_max], drawOpt='E2',
+                 attributes=mpl_attr, can=can, leg=leg, legEntries=mc_leg,
+                 legOpt='F', xLabel=x_label, yLabel=y_label)
+    can.SaveAs(args.output)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Script for creating pub '
+                                     'quality style plots')
+    parser.add_argument('histfile', help='root files containing the histograms '
+                        'in the structure as it is produced by the '
+                        'collect_histograms script')
+    parser.add_argument('-i', '--input', action='append',
+                        help='Specify one given input in the format: '
+                        'YYYY:TRG:DATAORMC:[:LEG], where the year, the trigger,'
+                        ' data or mc and the (optional) legend entry are '
+                        'separated by a colon')
+    parser.add_argument('-o', '--output', default='pub_plot.pdf',
+                        help='name of the output plot')
+    parser.add_argument('-v', '--variable', type=str, default='costh',
+                        help='variable to plot')
+    parser.add_argument('-f', '--frame', type=str, default='HX',
+                        help='reference frame')
+    parser.add_argument('-pt', '--ptbin', type=int, default=1,
+                        help='pt bin to plot')
+    # parser.add_argument('-y', '--ylabel', default='#chi_{c2} / #chi_{c1}',
+    #                     help='ylabel of the plot')
+    # parser.add_argument('-x', '--xlabel', default='|cos#theta^{HX}|',
+    #                     help='xlabel of the plot')
+    parser.add_argument('-l', '--legend', action='store_true', default=False,
+                        help='put legend onto the plot')
+    parser.add_argument('-nz', '--normalize-at-zero', default=False,
+                        action='store_true', help='Normalize distribution plots'
+                        ' at their value at 0. Default is to normalize by the '
+                        'sum of the chic1 and chic2 Integrals')
+
+    plot_type = parser.add_mutually_exclusive_group(required=True)
+    plot_type.add_argument('-r', '--ratio', action='store_true',
+                           help='Make ratio plot')
+    plot_type.add_argument('-d', '--dist', action='store_true',
+                           help='Make dist plot')
+
+    clargs = parser.parse_args()
+    main(clargs)

python/utils/PlotServer.py
@@ -1,0 +1,115 @@
+#!/usr/bin/env python
+"""
+Module that provides a plot server that retrieves plot from a root file
+"""
+
+import ROOT as r
+r.PyConfig.IgnoreCommandLineOptions = True
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s: %(message)s')
+
+from decorator import decorator
+
+from utils.misc_helpers import get_full_trigger
+
+def try_file_access(proto_err_msg):
+    """
+    Decorator for trying to access the root file 'safely'
+    """
+    @decorator
+    def access_wrapper(func, *args):
+        """Decorator that will be returned"""
+        def try_access(*args):
+            """Function that tries the access and returns obj or None"""
+            obj_or_dir = func(*args)
+            if not obj_or_dir:
+                logging.warning('plot server access: ' +
+                                proto_err_msg.format(*args))
+                return None
+            return obj_or_dir
+        return try_access(*args)
+
+    return access_wrapper
+
+
+class PlotServer(object):
+    """
+    Simple plot server that retrieves and returns TObjects from a TFile with an
+    internal TDirectory structure.
+    """
+    def __init__(self, filename):
+        """
+        Args:
+            filename (str): path to the rootfile from which objects should be
+                obtained
+        """
+        self.hfile = r.TFile.Open(filename)
+
+
+    @try_file_access('Error in retrieving hist: year={1}, trigger={2}, pt={3}, '
+                     'var={4}, frame={5}, plot={6}, dmc={7}')
+    def get_hist(self, dmc, year, trigger, pt, var, frame, plot):
+        """
+        Get a histogram from a file where the internal directory structure uses
+        the data/mc, year, trigger and pt information and the plots are
+        identified by the variable name, the reference frame and a plot
+        identifier.
+
+        Args:
+            dmc (str): 'data' or 'mc' identifier
+            year (int or str):
+            trigger (str): minimum part of a trigger that uniquely identifies it
+            pt (int or str): pt bin
+            var (str): the variable (e.g. 'costh' or 'phi')
+            frame (str): reference frame (in capitals)
+            plot (str): which plot (e.g. 'chic1' or 'ratio')
+
+        Returns:
+            ROOT.TH1: TH1D found at location specified by arguments or None
+        """
+        logging.debug('Trying to get plot from file {0}, Parameters are: '
+                      'year={1}, trigger={2}, pt={3}, var={4}, frame={5}, '
+                      'plot={6}, dmc={7}'.format(self.filename(), year, trigger,
+                                                 pt, var, frame, plot, dmc))
+        full_trigger = get_full_trigger(trigger)
+        subdir = '/'.join([dmc, str(year), full_trigger, str(pt)])
+        histname = '_'.join([plot, var, frame])
+        return self._get_by_str('/'.join([subdir, histname]))
+
+
+    @try_file_access('Directory {1}/{2}/{3} not found')
+    def get_dir_contents(self, dmc, year, trigger, pt):
+        """
+        TODO: doc
+        """
+        logging.debug('Trying to get directory, Parameters are: year={}, '
+                      'trigger={}, pt={}, dmc={}'
+                      .format(year, trigger, pt, dmc))
+        return self._get_by_str('/'.join([dmc, year, trigger, pt]))
+
+
+    def filename(self):
+        """
+        Get the filename from which the objects are served
+
+        Returns:
+            str: filename
+        """
+        return self.hfile.GetName()
+
+
+    def _get_by_str(self, obj_str):
+        """
+        Get an object from the histo file via a string describing the path to it
+
+        Args:
+            obj_str (str): String with path to desired object
+
+        Returns:
+            ROOT.TObject found at the place pointed to by obj_str
+        """
+        logging.debug('Trying to get object {} from file {}'
+                      .format(obj_str, self.filename()))
+        return self.hfile.Get(obj_str)

python/utils/setup_plot_style.py
@@ -142,8 +142,8 @@ def set_TDR_style():
     # tdr_style.SetTimeOffset(Double_t toffset)
     # tdr_style.SetHistMinimumZero(true)
 
-    tdr_style.SetHatchesLineWidth(5)
-    tdr_style.SetHatchesSpacing(0.05)
+    tdr_style.SetHatchesLineWidth(1)
+    tdr_style.SetHatchesSpacing(0.5)
 
     tdr_style.cd()
 

commit 9bb4e7adf1c1b03485d9960f5cbfc46166072d65

Fix bug introduced by b5eded1, where default argument for drawOpt was removed accidentally

python/utils/plot_helpers.py
@@ -155,7 +155,7 @@ def plot_on_canvas(can, plots, **kwargs):
     get_att = lambda i: attributes[ i % len(attributes) ]
 
     leg_option = kwargs.pop('legOpt', '')
-    draw_option = kwargs.pop('drawOpt')
+    draw_option = kwargs.pop('drawOpt', '')
     if not leg_option:
         leg_option = draw_option
     draw_option = ''.join(['same', draw_option])