6dff08d895007737ff93c8e32c68452d3b438e2d
tmadlener <thomas.madlener@cern.ch>
2018-02-23 18:05:34 +0100

    Move sourceing of bash helper function file into if clause
    Since the path to the bash_function.sh file is relative it will not be sourced
    if the setup script is not called from the base directory

--- setup.sh
+++ setup.sh
@@ -2,10 +2,6 @@
 ## setup script for setting up some environment and path variables.
 ## TODO: Find common setup, that works with all the python packages and ROOT etc. and put the initialization here.
 
-
-## get the function definitions (unconditionally)
-source ./scripts/bash_functions.sh
-
 # first check if setup already happened, and only do it if not or if --force flag is passed.
 # In this way, this script can be sourced multiple times or from within bash scripts
 if [[ -z "${CHIB_CHIC_POLFW_DIR+x}" ]] || $(check_args_flag "--force" ${@}); then
@@ -22,6 +18,9 @@ if [[ -z "${CHIB_CHIC_POLFW_DIR+x}" ]] || $(check_args_flag "--force" ${@}); the
     export ROOT_CONFIG_BIN=$(which root-config)
     export GCC_BASE_DIR=""
 
+
+    ## get the bash helper function definitions
+    source ${CHIB_CHIC_POLFW_DIR}/scripts/bash_functions.sh
 
     fetch_json_header ${@}
 fi
