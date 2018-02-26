0b99fb3c398bd0a036e7f95df99ad637d6ca19d7
thomas madlener <thomas.madlener@cern.ch>
2018-02-26 12:22:30 +0100

    Add function to shift TGraphs and do slight refactoring

--- python/utils/graph_utils.py
+++ python/utils/graph_utils.py
@@ -9,6 +9,61 @@ import ROOT as r
 
 from utils.misc_helpers import get_vals_from_rwbuffer
 
+def get_errors(graph):
+    """
+    Get the errors of the passed in graph
+
+    Args:
+        graph (ROOT.TGraphErrors or ROOT.TGraphAsymmErrors): graph for which
+            errors should be obtained
+
+    Returns:
+        tuple: Tuple containing numpy.arrays with all the error values. If
+            graph was a TGraphErors two elements are returned (x, y). If graph
+            was a TGraphAsymmErrors four elements are returend
+            (x_lo, x_hi, y_lo, y_hi)
+    """
+    n_points = graph.GetN()
+
+    # have to test from specific to less specific here
+    if graph.InheritsFrom('TGraphAsymmErrors'):
+        x_lo_errs = get_vals_from_rwbuffer(graph.GetEXlow(), n_points)
+        x_hi_errs = get_vals_from_rwbuffer(graph.GetEXhigh(), n_points)
+        y_lo_errs = get_vals_from_rwbuffer(graph.GetEYlow(), n_points)
+        y_hi_errs = get_vals_from_rwbuffer(graph.GetEYhigh(), n_points)
+        return x_lo_errs, x_hi_errs, y_lo_errs, y_hi_errs
+    if graph.InheritsFrom('TGraphErrors'):
+        x_errs = np.array(graph.GetEX())
+        y_errs = np.array(graph.GetEY())
+        return x_errs, y_errs
+
+
+def shift_graph(graph, shift):
+    """
+    Shift the central y-values of a graph
+
+    Args:
+        graph (ROOT.TGraph or inheriting) graph to shift
+        shift (float): shift value by which the y-value of the graph should be
+            shifted
+
+    Returns:
+        ROOT.TGraph: graph of the same type as the passed in graph shifted by
+            a constant factor
+    """
+    x_vals = np.array(graph.GetX())
+    n_points = graph.GetN()
+
+    y_vals = np.array(graph.GetY()) + shift
+
+    # have to test from specific to less specific here
+    if graph.InheritsFrom('TGraphAsymmErrors'):
+        return r.TGraphAsymmErrors(n_points, x_vals, y_vals, *get_errors(graph))
+    if graph.InheritsFrom('TGraphErrors'):
+        return r.TGraphErrors(n_points, x_vals, y_vals, *get_errors(graph))
+
+    return r.TGraph(n_points, x_vals, y_vals)
+
 
 def scale_graph(graph, scale):
     """
@@ -29,16 +84,15 @@ def scale_graph(graph, scale):
 
     # have to test from very specific to less specific here
     if graph.InheritsFrom('TGraphAsymmErrors'):
-        x_lo_errs = get_vals_from_rwbuffer(graph.GetEXlow(), n_points)
-        x_hi_errs = get_vals_from_rwbuffer(graph.GetEXhigh(), n_points)
-        y_lo_errs = get_vals_from_rwbuffer(graph.GetEYlow(), n_points) * scale
-        y_hi_errs = get_vals_from_rwbuffer(graph.GetEYhigh(), n_points) * scale
+        x_lo_errs, x_hi_errs, y_lo_errs, y_hi_errs = get_errors(graph)
+        y_lo_errs *= scale
+        y_hi_errs *= scale
         return r.TGraphAsymmErrors(n_points, x_vals, y_vals,
                                    x_lo_errs, x_hi_errs, y_lo_errs, y_hi_errs)
 
     if graph.InheritsFrom('TGraphErrors'):
-        x_errs = np.array(graph.GetEX())
-        y_errs = np.array(graph.GetEY()) * scale
+        x_errs, y_errs = get_errors(graph)
+        y_errs *= scale
         return r.TGraphErrors(n_points, x_vals, y_vals, x_errs, y_errs)
 
     return r.TGraph(n_points, x_vals, y_vals)
