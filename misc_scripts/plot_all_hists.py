dc4649fe27f4312c81578a24dc60200a498cbef6
thomas madlener <thomas.madlener@cern.ch>
2018-11-21 17:14:12 +0100

    Implement possibility to do ProjectionX and Y as well as toggle verbose output

--- misc_scripts/plot_all_hists.py
+++ misc_scripts/plot_all_hists.py
@@ -3,9 +3,15 @@
 Script that simply stores all histograms in a root file
 """
 
+import sys
+
 import ROOT as r
 r.PyConfig.IgnoreCommandLineOptions = True
 r.gROOT.SetBatch()
+
+import logging
+logging.basicConfig(level=logging.ERROR,
+                    format='%(levelname)s - %(funcName)s: %(message)s')
 
 from utils.plot_helpers import mkplot
 from utils.misc_helpers import cond_mkdir
@@ -21,23 +27,41 @@ def store_hist(hist, out_dir, formats, draw_opt, **kwargs):
             haxis.SetRangeUser(*range_vals)
 
 
+    hist_cmd = kwargs.pop('hist_cmd', None)
+    if hist_cmd is not None:
+        if hasattr(hist, hist_cmd):
+            hist = getattr(hist, hist_cmd)()
+            if not isinstance(hist, (r.TH1, r.TGraph, r.TF1)):
+                logging.fatal('Result of \'{}\' is not a plotable object'
+                              .format(hist_cmd))
+                sys.exit(1)
+        else:
+            logging.error('Cannot call \'{}\' on histogram object.'
+                          .format(hist_cmd))
+
     hist.SetStats(0)
     can = mkplot(hist, drawOpt=draw_opt)
 
-    name ='/'.join([out_dir, hist.GetName()])
+    name = '/'.join([out_dir, hist.GetName()])
     for form in formats:
         can.SaveAs('.'.join([name, form]))
 
 
 def main(args):
     """Main"""
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+    else:
+        r.gROOT.ProcessLine('gErrorIgnoreLevel = 1001')
+
     histfile = r.TFile.Open(args.inputfile)
     cond_mkdir(args.outdir)
 
     formats = args.extensions.split(',')
     for hist in list_obj(histfile, 'TH1', args.filter):
         store_hist(histfile.Get(hist), args.outdir, formats, args.draw_opt,
-                   xrange=args.xrange, yrange=args.yrange, zrange=args.zrange)
+                   xrange=args.xrange, yrange=args.yrange, zrange=args.zrange,
+                   hist_cmd=args.hist_cmd)
 
 
 if __name__ == '__main__':
@@ -60,7 +84,10 @@ if __name__ == '__main__':
                         'plot', default=None)
     parser.add_argument('-xr', '--xrange', help='Constrain the x-range of a '
                         'plot', default=None)
-
-
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='Enable printouts to screen')
+    parser.add_argument('-c', '--hist-cmd', help='Call this function (input is '
+                        'the name as string) on each histogram and plot the '
+                        'result', default=None)
     clargs = parser.parse_args()
     main(clargs)
