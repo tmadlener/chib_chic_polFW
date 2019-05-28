Fix small bug in chi2 calculation
If the difference between graph and prediction is larger than zero then the
graph is above the prediction, meaning that the lower uncertainties should be
used. Vice versa for a difference smaller than 0.

python/utils/graph_utils.py
@@ -579,7 +579,7 @@ def fit_to_graph(graph, template):
         pred = norm * tval
         diff = gval - pred
         # If the prediction is below the graph use low uncertainties else high
-        err = (diff < 0) * elow + (diff > 0) * ehigh
+        err = (diff > 0) * elow + (diff < 0) * ehigh
         return np.sum(diff**2 / err**2)
 
     mres = minimize(_chisquare, (tval / gval)[0], method='nelder-mead')