4437e219ee5de58b94bcdf1aa587a43a077c5520
tmadlener <thomas.madlener@cern.ch>
2019-06-03 16:19:55 +0200

    Add possibility to generate debug plots from combination

--- polarization/plotting/combine_ppds.py
+++ polarization/plotting/combine_ppds.py
@@ -3,17 +3,22 @@
 Script to combine PPDs into a final PPD
 """
 
+from os.path import dirname, basename
 from itertools import combinations
 
 import numpy as np
 
 import ROOT as r
 r.PyConfig.IgnoreCommandLineOptions = True
+r.gROOT.SetBatch()
 
-from utils.hist_utils import get_binning, get_array, from_array
-from utils.data_handling import list_obj
+from utils.hist_utils import get_binning, get_array, from_array, rebin
+from utils.setup_plot_style import set_basic_style
+from utils.plot_helpers import set_color, mkplot, default_attributes
 
-from common_func import get_scaled_ppd, get_scaled_ppd_2d
+from common_func import (
+    get_scaled_ppd, get_scaled_ppd_2d, plot_lth, plot_dlth, plot_norm
+)
 
 
 def get_combined_ppd(inputfiles, var):
@@ -50,8 +55,27 @@ def get_combined_ppd_2d(inputfiles, var1, var2):
     return from_array(np.max(ppd_vals, axis=0), ppd_binning)
 
 
+PLOT_FUNCTIONS = {
+    'lth': plot_lth, 'dlth': plot_dlth, 'norm': plot_norm
+}
+
+def make_debug_plot(comb_ppd, inputfiles, var):
+    """
+    Make a plot overlaying the combined ppd and the input ppds
+    """
+    can = PLOT_FUNCTIONS[var](rebin(comb_ppd, [(0, 200)]))
+    set_color(can.pltables[1], 1) # make the combined ppd black
+
+    ppds = [get_scaled_ppd(f, var, 200) for f in inputfiles]
+    mkplot(ppds, can=can, drawOpt='samehist',
+           attr=default_attributes(linewidth=1))
+
+    return can
+
+
 def main(args):
     """Main"""
+    set_basic_style()
     infiles = [r.TFile.Open(f) for f in args.inputfiles]
 
     outfile = r.TFile(args.outfile, 'recreate')
@@ -61,6 +85,12 @@ def main(args):
         comb_ppd = get_combined_ppd(infiles, var)
         comb_ppd.SetName('ppd_1d_{}'.format(var))
         comb_ppd.Write()
+
+        if args.debug_plots:
+            outdir = dirname(args.outfile)
+            filebase = basename(args.outfile).replace('.root', '')
+            can = make_debug_plot(comb_ppd, infiles, var)
+            can.SaveAs('{}/{}_comb_ppd_{}.pdf'.format(outdir, filebase, var))
 
     for var1, var2 in combinations(['lth', 'dlth', 'norm'], 2):
         comb_ppd = get_combined_ppd_2d(infiles, var1, var2)
@@ -77,6 +107,8 @@ if __name__ == '__main__':
                         'stored')
     parser.add_argument('inputfiles', nargs='+', help='PPD scan files containing'
                         ' histograms that are used for combining')
+    parser.add_argument('--debug-plots', action='store_true', default=False,
+                        help='Create some debug overview plots of the 1d ppds')
 
 
     clargs = parser.parse_args()
