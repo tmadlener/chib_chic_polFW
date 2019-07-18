tmadlener/chib_chic_polFW

commit 07be4ab59d4d4b0fdf4de95ed2fbc939f89c5dda

Make lambda_bkg plot compliant with the model definition.
- Updated the script making the proto param plots to automatically increase the
  pre-defined range if it doesn't fit all the plotted points.
- Added units to the fitted parameters
- Updated functions determining min/max values of graphs to also consider
  uncertainties.
- Exposed "dscale" parameter to mkplot interface

python/utils/graph_utils.py
@@ -308,7 +308,18 @@ def _get_y_max_graph(graph):
     """
     Get the maximum y value (discarding uncertainties) of a graph (or graphs)
     """
-    max_vals = [np.max(np.array(g.GetY())) for g in make_iterable(graph)]
+    max_vals = []
+    for graph in make_iterable(graph):
+        yvals = np.array(graph.GetY())
+        if isinstance(graph, r.TGraphAsymmErrors):
+            _, _, _, yerr = get_errors(graph)
+        elif isinstance(graph, r.TGraphErrors):
+            _, yerr = get_errors(graph)
+        else:
+            yerr = np.zeros(graph.GetN())
+
+        max_vals.append(np.max(yvals + yerr))
+
     return np.max(max_vals)
 
 
@@ -316,7 +327,18 @@ def _get_y_min_graph(graph):
     """
     Get the minimum y value (discarding uncertainties) of a graph (or graphs)
     """
-    min_vals = [np.min(np.array(g.GetY())) for g in make_iterable(graph)]
+    min_vals = []
+    for graph in make_iterable(graph):
+        yvals = np.array(graph.GetY())
+        if isinstance(graph, r.TGraphAsymmErrors):
+            _, _, yerr, _ = get_errors(graph)
+        elif isinstance(graph, r.TGraphErrors):
+            _, yerr = get_errors(graph)
+        else:
+            yerr = np.zeros(graph.GetN())
+
+        min_vals.append(np.min(yvals - yerr))
+
     return np.min(min_vals)
 
 
@@ -324,7 +346,18 @@ def _get_x_max_graph(graph):
     """
     Get the maximum x value (discarding uncertainties) of a graph (or graphs)
     """
-    max_vals = [np.max(np.array(g.GetX())) for g in make_iterable(graph)]
+    max_vals = []
+    for graph in make_iterable(graph):
+        xvals = np.array(graph.GetX())
+        if isinstance(graph, r.TGraphAsymmErrors):
+            _, xerr, _, _= get_errors(graph)
+        elif isinstance(graph, r.TGraphErrors):
+            xerr, _ = get_errors(graph)
+        else:
+            xerr = np.zeros(graph.GetN())
+
+        max_vals.append(np.max(xvals + xerr))
+
     return np.max(max_vals)
 
 
@@ -332,7 +365,18 @@ def _get_x_min_graph(graph):
     """
     Get the minimum x value (discarding uncertainties) of a graph (or graphs)
     """
-    min_vals = [np.min(np.array(g.GetX())) for g in make_iterable(graph)]
+    min_vals = []
+    for graph in make_iterable(graph):
+        xvals = np.array(graph.GetX())
+        if isinstance(graph, r.TGraphAsymmErrors):
+            xerr, _, _, _= get_errors(graph)
+        elif isinstance(graph, r.TGraphErrors):
+            xerr, _ = get_errors(graph)
+        else:
+            xerr = np.zeros(graph.GetN())
+
+        min_vals.append(np.min(xvals - xerr))
+
     return np.min(min_vals)
 
 

misc_scripts/make_proto_param_plots.py
@@ -4,14 +4,18 @@ Script to make the plots of the proto parameters of the simultaneous binned fits
 """
 
 import re
+import logging
+logging.basicConfig(level=logging.WARNING,
+                    format='%(levelname)s - %(funcName)s: %(message)s')
 
 import ROOT as r
 r.PyConfig.IgnoreCommandLineOptions = True
 r.gROOT.ProcessLine('gErrorIgnoreLevel = 1001')
 r.gROOT.SetBatch()
 
+
 from utils.plot_helpers import (
-    mkplot, default_attributes, setup_latex, put_on_latex
+    mkplot, default_attributes, setup_latex, put_on_latex, get_y_min, get_y_max
 )
 from utils.plot_decoration import YLABELS, FIX_RANGES, VAR_PLOT
 from utils.misc_helpers import cond_mkdir
@@ -47,10 +51,20 @@ def make_plot(pname, param_fg, bin_var):
     """
     Make plot for one parameter graph or function and return the canvas
     """
+    ymin, ymax = get_y_min(param_fg), get_y_max(param_fg)
+    yran = FIX_RANGES.get(pname, None)
+    if yran is not None:
+        if ymin < yran[0] or ymax > yran[1]:
+            logging.debug('Adapting predefined range = {} of {}'
+                          .format(yran, param_fg.GetName()))
+            yran[0] += ((ymin - yran[0]) * 1.1)
+            yran[1] += ((ymax - yran[1]) * 1.1)
+
     can = mkplot(param_fg, drawOpt='PE',
                  attr=default_attributes(open_markers=False),
                  yLabel=YLABELS.get(pname, pname),
-                 yRange=FIX_RANGES.get(pname, None),
+                 yRange=yran,
+                 ydscale=0.15,
                  **VAR_PLOT[bin_var])
 
     # If TF1, simply assume that it is a polynomial for now and also put the

python/utils/plot_decoration.py
@@ -8,20 +8,20 @@ FIX_RANGES = {
     'CBmass2': [3.55, 3.5525],
     'CBsigma1': [0.004, 0.006],
     'CBsigma2': [0.0055, 0.009],
-    'lambda_bkg': [-3.0, -1.7],
+    # 'lambda_bkg': [-3.0, -1.7],
     'mu_bkg': [3.2, 3.25],
     'sigma_bkg': [0.05, 0.13],
     'r_chic0_chic1': [0.01, 0.06],
 }
 
 YLABELS = {
-    'CBmass1': '#mu^{#chi_{c1}}',
-    'CBmass2': '#mu^{#chi_{c2}}',
-    'CBsigma1': '#sigma^{#chi_{c1}}',
-    'CBsigma2': '#sigma^{#chi_{c2}}',
-    'mu_bkg': '#mu^{Bkg}',
-    'sigma_bkg': '#sigma^{Bkg}',
-    'lambda_bkg': '#lambda^{Bkg}',
+    'CBmass1': '#mu^{#chi_{c1}} / GeV',
+    'CBmass2': '#mu^{#chi_{c2}} / GeV',
+    'CBsigma1': '#sigma^{#chi_{c1}} / GeV',
+    'CBsigma2': '#sigma^{#chi_{c2}} / GeV',
+    'mu_bkg': '#mu^{Bkg} / GeV',
+    'sigma_bkg': '#sigma^{Bkg} / GeV',
+    'lambda_bkg': '#minus 1 / #lambda^{Bkg} / GeV^{-1}',
     'Nchic1': 'N^{#chi_{c1}}',
     'Nchic2': 'N^{#chi_{c2}}',
     'Nchic0': 'N^{#chi_{c0}}',

python/utils/plot_helpers.py
@@ -388,12 +388,13 @@ def _setup_plot_hist(can, pltables, **kwargs):
         # No frame needed
         return None
 
-    def get_ax_range(plot_range, ax_range):
+    def get_ax_range(plot_range, ax_range, dscale=None):
         """Get the axis default range or the plot range"""
         # depending on the sign of the min and maximum values it is necessary
         # to either add or subtract from the values to have the widening in
         # range go into the right direction
-        dscale = 0.1
+        if dscale is None:
+            dscale = 0.1
         dmin = lambda val: dscale * val if val < 0 else -dscale * val
         dmax = lambda val: dscale * val if val > 0 else -dscale * val
 
@@ -413,10 +414,10 @@ def _setup_plot_hist(can, pltables, **kwargs):
     # Only compute the range from the plots if it is really necessary
     if None in x_range:
         x_plot = (get_x_min(pltables), get_x_max(pltables))
-        x_range = get_ax_range(x_plot, x_range)
+        x_range = get_ax_range(x_plot, x_range, kwargs.pop('xdscale', None))
     if None in y_range:
         y_plot = (get_y_min(pltables), get_y_max(pltables))
-        y_range = get_ax_range(y_plot, y_range)
+        y_range = get_ax_range(y_plot, y_range, kwargs.pop('ydscale', None))
 
     plot_hist = can.DrawFrame(x_range[0], y_range[0], x_range[1], y_range[1])
 
@@ -449,6 +450,10 @@ def mkplot(pltables, **kwargs):
             a None value or even two None values. For any passed value that
             value will be used as axis range, for any None value an appropriate
             value will be determined from the passed pltables
+        [xy]dscale (float, optional): Factor that is used to extend the plot
+            range using the min / max observed value in the plots. The plot
+            range will be (min * (1 - dscale), max * (1 - dscale)). The [xy]Range
+            takes precedence over this one.
         can (ROOT.TCanvas): Do not create new canvas but use passed canvas to
             plot on
         log[xyz] (boolean, optional): Set the [xyz] axis to log scale

commit 42a8daf0f23abc47e2ecd6792638f8a1e390e321

Fix tests that were broken by 07be4ab

python/utils/graph_utils.py
@@ -306,7 +306,7 @@ def get_lower_band(graph):
 
 def _get_y_max_graph(graph):
     """
-    Get the maximum y value (discarding uncertainties) of a graph (or graphs)
+    Get the maximum y value of a graph (or graphs)
     """
     max_vals = []
     for graph in make_iterable(graph):
@@ -325,7 +325,7 @@ def _get_y_max_graph(graph):
 
 def _get_y_min_graph(graph):
     """
-    Get the minimum y value (discarding uncertainties) of a graph (or graphs)
+    Get the minimum y value of a graph (or graphs)
     """
     min_vals = []
     for graph in make_iterable(graph):
@@ -344,7 +344,7 @@ def _get_y_min_graph(graph):
 
 def _get_x_max_graph(graph):
     """
-    Get the maximum x value (discarding uncertainties) of a graph (or graphs)
+    Get the maximum x value of a graph (or graphs)
     """
     max_vals = []
     for graph in make_iterable(graph):
@@ -363,7 +363,7 @@ def _get_x_max_graph(graph):
 
 def _get_x_min_graph(graph):
     """
-    Get the minimum x value (discarding uncertainties) of a graph (or graphs)
+    Get the minimum x value of a graph (or graphs)
     """
     min_vals = []
     for graph in make_iterable(graph):

python/test/test_graph_utils.py
@@ -252,5 +252,36 @@ class TestDivideGraphs(unittest.TestCase):
         npt.assert_allclose(y_err, an_err)
 
 
+class TestGetErrors(unittest.TestCase):
+    def test_tgraph_errors(self):
+        graph = get_random_graph(r.TGraphErrors)
+        xerr, yerr = gu.get_errors(graph)
+
+        root_x_err, root_y_err = [], []
+        for i in xrange(graph.GetN()):
+            root_x_err.append(graph.GetErrorX(i))
+            root_y_err.append(graph.GetErrorY(i))
+
+        npt.assert_allclose(np.array(root_x_err), xerr)
+        npt.assert_allclose(np.array(root_y_err), yerr)
+
+
+    def test_tgraph_asymm_errors(self):
+        graph = get_random_graph(r.TGraphAsymmErrors)
+        xlo, xhi, ylo, yhi = gu.get_errors(graph)
+
+        rxlo, rxhi, rylo, ryhi = [], [], [], []
+        for i in xrange(graph.GetN()):
+            rxlo.append(graph.GetErrorXlow(i))
+            rxhi.append(graph.GetErrorXhigh(i))
+            rylo.append(graph.GetErrorYlow(i))
+            ryhi.append(graph.GetErrorYhigh(i))
+
+        npt.assert_allclose(np.array(rxlo), xlo)
+        npt.assert_allclose(np.array(rxhi), xhi)
+        npt.assert_allclose(np.array(rylo), ylo)
+        npt.assert_allclose(np.array(ryhi), yhi)
+
+
 if __name__ == '__main__':
     unittest.main()

python/test/test_plot_helpers.py
@@ -10,6 +10,7 @@ r.PyConfig.IgnoreCommandLineOptions = True
 import numpy as np
 
 from utils.misc_helpers import create_random_str
+from utils.graph_utils import get_errors
 
 import utils.plot_helpers as ph
 
@@ -24,18 +25,22 @@ class TestGetXYMinMax(unittest.TestCase):
         self.func_y_min = -11
         self.func_y_max = 1.5
 
-        # Use TGraphAsymmErrors with random errors since they are anyhow ignored
+        # Use TGraphAsymmErrors with random errors
         self.graph = r.TGraphAsymmErrors(10, np.linspace(0, 4, 10),
                                          np.sqrt(np.linspace(4, 16, 10)),
                                          np.random.uniform(size=10),
                                          np.random.uniform(size=10),
                                          np.random.uniform(size=10),
                                          np.random.uniform(size=10))
+        gxlo, gxhi, gylo, gyhi = get_errors(self.graph)
 
-        self.graph_x_max = 4
-        self.graph_x_min = 0
-        self.graph_y_max = 4
-        self.graph_y_min = 2
+        graph_x = np.array(self.graph.GetX())
+        graph_y = np.array(self.graph.GetY())
+
+        self.graph_x_max = np.max(graph_x + gxhi)
+        self.graph_x_min = np.min(graph_x - gxlo)
+        self.graph_y_max = np.max(graph_y + gyhi)
+        self.graph_y_min = np.min(graph_y - gylo)
 
         self.hist = r.TH1D(create_random_str(8), '', 10, -3, 1)
         self.hist.Fill(-1)