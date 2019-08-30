89191409871896a29ea73404b09e2fedc1434814
thomas madlener <thomas.madlener@cern.ch>
2019-08-30 16:58:02 +0200

    Make it possible to specify the CLs that should be put on the plot

--- polarization/plotting/make_pt_dep_plot.py
+++ polarization/plotting/make_pt_dep_plot.py
@@ -58,8 +58,7 @@ def remove_y_errors(graph):
     return r.TGraphAsymmErrors(len(x_vals), x_vals, y_vals, x_lo, x_hi)
 
 
-
-def make_uncer_plot(graphs, variable):
+def make_uncer_plot(graphs, variable, levels):
     """
     Make the plot plotting the uncertainties and return the canvas
     """
@@ -74,7 +73,8 @@ def make_uncer_plot(graphs, variable):
                  yRange=YRANGES[variable], yLabel=YLABELS[variable],
                  leg=leg, legEntries=['central'], legOpt='P')
 
-    for n_sig, graph in graphs.iteritems():
+    for n_sig in levels:
+        graph = graphs[n_sig]
         mkplot(graph, can=can, drawOpt='E2same', legOpt='F', attr=ATTR[n_sig],
                leg=leg, legEntries=[LEG_KEYS[n_sig]])
 
@@ -103,12 +103,12 @@ def make_limit_plot(graphs, variable):
     return can
 
 
-def make_plot(graphs, variable):
+def make_plot(graphs, variable, levels):
     """
     Make the graph and return the canvas
     """
     if variable in ['dlth', 'dlph']:
-        return make_uncer_plot(graphs, variable)
+        return make_uncer_plot(graphs, variable, levels)
     else:
         return make_limit_plot(graphs, variable)
 
@@ -118,8 +118,7 @@ def main(args):
     set_TDR_style()
     graphfile = r.TFile.Open(args.graphfile)
     var, graphs = get_graphs(graphfile)
-
-    can = make_plot(graphs, var)
+    can = make_plot(graphs, var, args.sigmas.split(','))
     cond_mkdir(args.outdir)
     can.SaveAs('{}/{}_v_pt.pdf'.format(args.outdir, var))
 
@@ -133,6 +132,10 @@ if __name__ == '__main__':
                         '1, 2 and 3 sigma uncertainty bands')
     parser.add_argument('-o', '--outdir', help='Output directory into which the '
                         'plot will be stored', default='.')
+    parser.add_argument('-n', '--sigmas', help='Comma separated list of '
+                        'uncertainty levels that should be plotted. NOTE: '
+                        'can only be 1, 2 or 3 (or any combination of them)',
+                        default='1')
 
     clargs = parser.parse_args()
     main(clargs)
