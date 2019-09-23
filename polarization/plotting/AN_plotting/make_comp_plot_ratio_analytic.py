ae00b571dfc869774998206028d711d1b4123216
thomas madlener <thomas.madlener@cern.ch>
2019-09-23 13:46:02 +0200

    Late commit of changes for plots used in last update of AN
    Script compares analytic shapes corresponding to specific scenarios with the
    fitted (and corrected) ratios. Depending on the input arguments different
    comparison plots are produced

--- polarization/plotting/AN_plotting/make_comp_plot_ratio_analytic.py
+++ polarization/plotting/AN_plotting/make_comp_plot_ratio_analytic.py
@@ -11,7 +11,8 @@ r.gROOT.SetBatch()
 from utils.plot_helpers import mkplot, default_attributes, setup_legend
 from utils.setup_plot_style import add_auxiliary_info, set_TDR_style
 from utils.plot_decoration import YLABELS, VAR_PLOT
-from utils.misc_helpers import cond_mkdir, create_random_str, fmt_float
+from utils.misc_helpers import cond_mkdir_file, create_random_str, fmt_float
+from utils.graph_utils import pull_graph
 
 import utils.misc_helpers
 utils.misc_helpers.MAX_N_DIGITS = 1
@@ -60,20 +61,31 @@ def fit_to_dist(func, dist):
     return fitres.Chi2(), fitres.Ndf()
 
 
-def get_analytic_scenarios_costh(dlth=None):
+def get_analytic_scenarios_costh(dlth=None, lam1=None, lam2=None,
+                                 no_extremes=False):
     """
     Get analytic shapes for different polarization scenarios
 
     If dlth is not None, the value of dlth will be used with different values
     for lambda 1. If it is None, then the extreme polarization scenarios will be
     used instead
+
+    If lam1 AND lam2 are passed a scenario specifically for these values will
+    also be added
     """
     if dlth is not None:
-        lth1 = [0, 0.5, 1]
+        lth1 = [0, 0.5, 1.0]
         lth2 = [dlth + v for v in lth1]
     else:
-        lth1 = [0, 1, -1./3.]
-        lth2 = [0, -3./5., 1]
+        if no_extremes:
+            lth1, lth2 = [], []
+        else:
+            lth1 = [0, 1, -1./3.]
+            lth2 = [0, -3./5., 1]
+
+    if lam1 is not None and lam2 is not None:
+        lth1 = [0, lam1]
+        lth2 = [0, lam2]
 
     funcs = OrderedDict()
     for l1, l2 in zip(lth1, lth2):
@@ -114,6 +126,71 @@ def make_ratio_plot(ratio, analytic_scens, variable):
     return can
 
 
+def make_ratio_pull_plot(ratio, analytic_scens, variable):
+    """
+    Make a comparison plot with an additional pull panel at the bottom
+    """
+    # Scale all analytic scenarios first
+    chi2_ndfs = get_chi2_ndf(ratio, analytic_scens)
+
+    can = mkplot([]) # get a canvas that is already a TCanvasWrapper
+    can.cd()
+    rpad = r.TPad('ratio_pad', 'ratio pad', 0, 0.3, 1, 0.98)
+    r.SetOwnership(rpad, False)
+    rpad.SetBottomMargin(0)
+    rpad.Draw()
+
+    rpad = mkplot(ratio, drawOpt='PE', can=rpad,
+                  attr=[{'color': 1, 'size': 1, 'marker': 20}],
+                  yLabel=YLABELS.get('r_chic2_chic1'), yRange=[0.001, 0.75],
+                  **VAR_PLOT[variable])
+
+    leg = setup_legend(0.2, 0.15, 0.65, 0.32)
+    mkplot(analytic_scens.values(), drawOpt='sameL', can=rpad,
+           attr=ANALYTIC_ATTRS,
+           leg=leg, legEntries=analytic_scens.keys(), legOpt='L')
+
+
+    can.cd()
+    ppad = r.TPad('pull_pad', 'pull pad', 0, 0, 1, 0.3)
+    r.SetOwnership(ppad, False)
+    ppad.SetTopMargin(0)
+    ppad.SetBottomMargin(0.3)
+    ppad.Draw()
+
+    # Draw grid first so that it is not overlaying the plotted points
+    xran = VAR_PLOT[variable]['xRange']
+    ppad = mkplot(r.TLine(xran[0], 0, xran[1], 0), can=ppad,
+                  attr=[{'color': 1, 'width': 2, 'line': 1}],
+                  yLabel='pulls', yRange=[-4.99, 4.99],# grid=True,
+                  **VAR_PLOT[variable])
+    mkplot([r.TLine(xran[0], v, xran[1], v) for v in [-3, 3]], drawOpt='same',
+           can=ppad, attr=[{'color': 12, 'width': 2, 'line': 7}])
+
+    pulls = [pull_graph(ratio, s) for s in analytic_scens.values()]
+    ppad = mkplot(pulls, drawOpt='samePE', can=ppad,
+                  attr=default_attributes(open_markers=False),
+                  **VAR_PLOT[variable])
+
+
+    phist = ppad.pltables[0]
+    phist.SetNdivisions(505, "XYZ")
+    phist.SetLabelFont(42, "XYZ")
+    phist.SetLabelOffset(0.007, "XYZ")
+    phist.SetLabelSize(0.12, "XYZ")
+    phist.GetXaxis().SetTitleOffset(0.95)
+    phist.GetYaxis().SetTitleOffset(0.52)
+    phist.SetTitleSize(0.14, "XYZ")
+    # ppad.SetGridy()
+
+    can.add_tobject(rpad)
+    can.add_tobject(ppad)
+
+
+    add_auxiliary_info(can, 2012, prelim=True)
+    return can
+
+
 def main(args):
     """Main"""
     set_TDR_style()
@@ -122,22 +199,20 @@ def main(args):
     ratio = get_graph(rfile, 'r_chic2_chic1', args.variable)
 
     if args.variable == 'costh':
-        analytic_scens = get_analytic_scenarios_costh(args.delta_lambda)
+        analytic_scens = get_analytic_scenarios_costh(args.delta_lambda,
+                                                      args.lambda1, args.lambda2,
+                                                      args.no_extremes)
     else:
         # not implemented currently for phi direction
         analytic_scens = OrderedDict()
 
-    can = make_ratio_plot(ratio, analytic_scens, args.variable)
+    if args.pull_plot:
+        can = make_ratio_pull_plot(ratio, analytic_scens, args.variable)
+    else:
+        can = make_ratio_plot(ratio, analytic_scens, args.variable)
 
-    cond_mkdir(args.outdir)
-
-    # compute an additional part of the file name depending on the passed
-    # delta lambda
-    add = lambda x: '' if x is None else '_dlam_{:.2f}'.format(x)
-    add = add(args.delta_lambda)
-    add = add.replace('-', 'm').replace('.', 'p')
-    can.SaveAs('{}/r_chic2_chic1_v_{}_comp_analytic{}.pdf'
-               .format(args.outdir, args.variable, add))
+    cond_mkdir_file(args.outfile)
+    can.SaveAs(args.outfile)
 
 
 if __name__ == '__main__':
@@ -145,11 +220,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to make plots comparing'
                                      ' the ratio with analytical scenarios')
     parser.add_argument('ratiofile', help='File containing the ratio')
-    parser.add_argument('-o', '--outdir', help='Directory into which the '
-                        'produced plot is placed', default='.')
+    parser.add_argument('-o', '--outfile', help='Directory into which the '
+                        'produced plot is placed',
+                        default='r_chic2_chic1_comp_analytic.pdf')
     parser.add_argument('-d', '--delta-lambda', help='Delta lambda to be used '
                         'for the plots', default=None, type=float)
-
+    parser.add_argument('--lambda1', help='Lambda1 that should be used in the '
+                        'plot (can be used to enter an additional NRQCD'
+                        'scenario)', default=None, type=float)
+    parser.add_argument('--lambda2', help='Lambda2 that should be used in the '
+                        'plot (can be used to enter an additional NRQCD'
+                        'scenario)', default=None, type=float)
+    parser.add_argument('-n', '--no-extremes', help='Do not plot the extreme '
+                        'scenarios', default=False, action='store_true')
+    parser.add_argument('-p', '--pull-plot', help='Plot a pull panel',
+                        action='store_true', default=False)
 
     var_sel = parser.add_mutually_exclusive_group()
     var_sel.add_argument('--costh', action='store_const', dest='variable',
